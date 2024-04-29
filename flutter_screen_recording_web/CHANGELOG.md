ff9aeeafcdb79438fd89b121cb28b66e54dc5d2c
Mike <894659061@qq.com>
2024-04-29 15:30:51 +0800

    暂存

--- flutter_screen_recording_web/CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-## 1.0.2
-*Reorder codecs
-## 1.0.1
-*Change dependencies
-## 1.0.0
-*Initial release.
--- flutter_screen_recording_web/test/flutter_screen_recording_web_test.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_screen_recording_web/flutter_screen_recording_web.dart';
-
-void main() {}
